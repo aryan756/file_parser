7cea91ee38309f3c0f9cadd5cec7363d3da77c0a
Krishnam Garg <krishnamgarg0827@gmail.com>
2023-11-20 15:51:29 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,19 @@ use std::fs;
 struct Record {
     field1: String,
     field2: String,
+    field3: String,
+    field4: String,
+    field5: String,
+    field6: String,
+    field7: String,
+    field8: String,
+    field9: String,
     // Add more fields as per your CSV structure
 }
 
 fn parse_line(input: &str) -> IResult<&str, Record> {
     let (input, (field1, _, field2)) = tuple((alphanumeric, tag(","), alphanumeric))(input)?;
-    Ok((input, Record { field1: field1.to_string(), field2: field2.to_string() }))
+    Ok((input, Record { field1: field1.to_string(), field2: field2.to_string(),field3: field3.to_string(),field4: field4.to_string(),field5: field5.to_string(),field6: field6.to_string(),field7: field7.to_string(),field8: field8.to_string(),field9: field9.to_string() }))
 }
 
 fn main() {
